cd17cc4103ec358cb831893925bea9e5a36cfca4
Luana Ribeiro <luanasribeiro94@gmail.com>
2023-03-27 17:46:09 -0300

    Feat: adiciona validação do campo cpf

---
+++ js/script.js
@@ -0,0 +1,15 @@
+import verificaCPF from "./valida-cpf.js";
+
+// salvar todos os elementos com atributo de rerquired
+const camposDoFormulario = document.querySelectorAll("[required]");
+
+// para campo da lista vai ter um ouvinte do evento "blur" que chamará a função quando perder o foco no campo
+camposDoFormulario.forEach((campo) => {
+  campo.addEventListener("blur", () => verificaCampo(campo));
+});
+
+function verificaCampo(campo) {
+  if (campo.name == "cpf" && campo.value.length >= 11) {
+    verificaCPF(campo);   
+  }
+}
---
+++ js/valida-cpf.js
@@ -0,0 +1,64 @@
+export default function verificaCPF(campo) {
+  // método replace param 1 valor a substituir procurando onde tem . e -, param 2 é valor novo trocando por nada "" - removendo os caracteres
+  const cpf = campo.value.replace(/\.|-/g, "");
+  
+  if(validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
+    console.log("Esse cpf não existe");
+  } else {
+    console.log("Existe");
+  }
+}
+
+function validaNumerosRepetidos(cpf) {
+  const numerosRepetidos = [
+    '00000000000',
+    '11111111111',
+    '22222222222',
+    '33333333333',
+    '44444444444',
+    '55555555555',
+    '66666666666',
+    '77777777777',
+    '88888888888',
+    '99999999999'
+  ]
+  
+  // verifica se o cpf digitado é igual a um dos números repetidos, se for, retorna true e não é um cpf válido
+  return numerosRepetidos.includes(cpf);
+}
+
+function validaPrimeiroDigito(cpf) {
+  let soma = 0;
+  let multiplicador = 10;
+
+  for (let tamanho=0; tamanho <9; tamanho++) {
+    soma+=cpf[tamanho] * multiplicador;
+    multiplicador--;
+  }
+
+  soma = (soma*10) % 11;
+
+  if(soma == 10 || soma == 1) {
+    soma = 0;
+  }
+
+  return soma != cpf[9];
+}
+
+function validaSegundoDigito(cpf) {
+  let soma = 0;
+  let multiplicador = 11;
+
+  for (let tamanho=0; tamanho <10; tamanho++) {
+    soma+=cpf[tamanho] * multiplicador;
+    multiplicador--;
+  }
+
+  soma = (soma*10) % 11;
+
+  if(soma == 10 || soma == 1) {
+    soma = 0;
+  }
+
+  return soma != cpf[10];
+}
